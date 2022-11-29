8152a1d00c61cbc37edb878f1bef7fdbd154d534
endsp13l <aleksey.gusev88@gmail.com>
2022-11-29 21:11:29 +0300

    QTE UI complete

---
+++ Assets/Scripts/UI/QteDefence/QteButtonType.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public enum ButtonType {Empty, Coin, Shield}
+
+public class QteButtonType : MonoBehaviour
+{
+    public ButtonType buttonType;
+}
---
+++ Assets/Scripts/UI/QteDefence/QtePanel.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class QtePanel : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _buttonPrefabs;
+    private int _shieldsCollected;
+    private int _coinsCollected;
+    private Vector3[] _buttonsGrid = new Vector3[28];
+
+    private void OnEnable()
+    {
+        InitializeButtonsGrid();
+        CreateButtons(_buttonsGrid);
+    }
+
+    private void OnDisable()
+    {
+        ResetRewards();
+    }
+
+    private void InitializeButtonsGrid()
+    {
+        int arrayIndex = 0;
+        for (int yRange = 240; yRange >= -300; yRange -= 90)
+        {
+            for (int xRange = -150; xRange <= 150; xRange += 100)
+            {
+                _buttonsGrid[arrayIndex] = gameObject.transform.TransformPoint(xRange, yRange, 0f);
+                arrayIndex++;
+            }
+        }
+    }
+
+    private void CreateButtons(Vector3[] grid)
+    {
+        foreach (var position in grid)
+        {
+            GameObject currentButton = Instantiate(_buttonPrefabs[Random.Range(0, _buttonPrefabs.Length)], position,
+                Quaternion.identity, gameObject.transform);
+
+            ButtonType buttonType = currentButton.gameObject.GetComponent<QteButtonType>().buttonType;
+
+            if (buttonType == ButtonType.Coin)
+                currentButton.GetComponent<Button>().onClick.AddListener(() => CoinButtonClick(currentButton));
+            else if (buttonType == ButtonType.Shield)
+                currentButton.GetComponent<Button>().onClick.AddListener(() => ShieldButtonClick(currentButton));
+        }
+    }
+
+    private void ResetRewards()
+    {
+        _shieldsCollected = 0;
+        _coinsCollected = 0;
+    }
+
+    private void CoinButtonClick(GameObject button)
+    {
+        _coinsCollected += 1;
+        Destroy(button);
+    }
+
+    private void ShieldButtonClick(GameObject button)
+    {
+        _shieldsCollected += 1;
+        Destroy(button);
+    }
+}
